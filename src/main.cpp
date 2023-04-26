365bf833cb9fdfab2c6c31b9ae9a86548558a1bc
caret <caret@caret.rs>
2023-04-25 23:54:56 -0500

    Can send and recieve data!

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <sys/socket.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
   // Start the program!
-  std::string addr = "239.0.0.1";
+  std::string addr = "127.0.0.1";
   int port = 33333;
-
+  bool type = false; // False is server
   // Parse cli flags
   for (int i = 1; i < argc; i++) {
     std::string arg = argv[i];
@@ -21,9 +22,16 @@ int main(int argc, char **argv) {
         // Get the next argument and set it to the port var
         port = std::atoi(argv[++i]);
       }
+    } else if (arg == "-s" || arg == "--server" || arg == "-c" ||
+               arg == "--client") {
+      if (arg == "-c" || arg == "--client") {
+        type = true;
+      }
     } else if (arg == "-h" || arg == "--help") {
       std::cout
           << "Usage: " << argv[0] << " [OPTIONS]\n"
+          << "  -s, --server      Server mode\n"
+          << "  -c, --client      Client mode\n"
           << "  -p, --port        Port number to use (default: 12345)\n"
           << "  -a, --address     Multicast group address to use (default: "
              "239.0.0.1)\n"
@@ -42,7 +50,7 @@ int main(int argc, char **argv) {
   }
   // Print arguments
   std::cout << "The multicast-address is set to: ";
-  std::cout << addr << ":" << port;
+  std::cout << addr << ":" << port << "\n";
 
   // Create the multicast server!!!
 
@@ -63,6 +71,88 @@ int main(int argc, char **argv) {
               << "'" << std::endl;
     std::exit(1);
   }
+  if (!type) {
+    // Bind the socket to the multicast address and port!
+    sockaddr_in saddr;
+    std::memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = inet_addr(addr.c_str());
+    saddr.sin_port = htons(port);
+    int bound = bind(sockfd, (sockaddr *)&saddr, sizeof(saddr));
+    if (bound < 0) {
+      std::cerr << "Error binding socket! Socket error '" << bound << "'"
+                << std::endl;
+      std::exit(1);
+    }
 
-  // Bind the socket to the multicast address and port!
+    // Receive and print incoming data
+    char buf[1024];
+    sockaddr_in client_addr;
+    socklen_t addrlen = sizeof(client_addr);
+    ssize_t nbytes = recvfrom(sockfd, buf, sizeof(buf), 0,
+                              (sockaddr *)&client_addr, &addrlen);
+    if (nbytes < 0) {
+      std::cerr << "Error receiving data" << std::endl;
+      std::exit(1);
+    }
+    std::cout << "Received " << nbytes << " bytes: " << std::string(buf, nbytes)
+              << std::endl;
+    // Close the socket
+    close(sockfd);
+
+    return 0;
+  } else if (type) {
+    // Uses ip and port given to connect to
+    // Client mode
+    // Create a UDP socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+      std::cerr << "Error creating socket! Error code '" << sockfd << "'"
+                << std::endl;
+      std::exit(1);
+    }
+
+    // Resolve host address
+    sockaddr_in saddr;
+    std::memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+    if (inet_pton(AF_INET, addr.c_str(), &saddr.sin_addr) <= 0) {
+      std::cerr << "Error resolving host address" << std::endl;
+      std::exit(1);
+    }
+
+    // Connect to the server
+    // Turns out you don't connect
+    /*
+    int srv = connect(sockfd, (sockaddr *)&addr, sizeof(addr));
+    if (srv < 0) {
+      std::cerr << "Error connecting to server! Error code '" << srv << "'"
+                << std::endl;
+      std::exit(1);
+    }
+    */
+    // Send a message to the server
+    const char *message = "Hello, server!";
+    ssize_t nbytes = sendto(sockfd, message, std::strlen(message), 0,
+                            (sockaddr *)&saddr, sizeof(saddr));
+    if (nbytes < 0) {
+      std::cerr << "Error sending message to server" << std::endl;
+      std::exit(1);
+    }
+
+    // Receive a response from the server
+    char buf[1024];
+    nbytes = recv(sockfd, buf, sizeof(buf), 0);
+    if (nbytes < 0) {
+      std::cerr << "Error receiving message from server" << std::endl;
+      std::exit(1);
+    }
+    std::cout << "Received " << nbytes << " bytes: " << std::string(buf, nbytes)
+              << std::endl;
+
+    // Close the socket
+    close(sockfd);
+    return 0;
+  }
 }
